0db04dd4f09099cc14be7a6d6e11a0289c44d802
devpiyush <48790098+devpiyush@users.noreply.github.com>
2019-12-09 19:44:12 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/** password generator .cpp
+/ this program can generate a password of
+/ any specified length using the valid
+/ characters for most of the websites
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+static const char alphanumber[] =
+"0123456789"
+"!@#$%^*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz";
+
+int size = sizeof(alphanumber) - 1;
+
+int main()
+{
+    // password length
+    int length = 10;
+
+    srand(time(0));
+    cout << "your password is : " << endl;
+    for(int i = 0; i<length; i++)
+    {
+        cout << alphanumber[rand() % ::size]; // scope resolution operator :: makes it possible to access global variable size
+    }
+
+    return 0;
+}
